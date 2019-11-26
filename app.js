6b5f1783b809ea43e20b571981e74934b78af598
andreitol <tolstoi.a@gmail.com>
2019-11-26 18:48:37 +0200

    make func for create task lyout (li)

--- app.js
+++ app.js
@@ -44,5 +44,32 @@ const tasks = [
       console.error('Передайте список задач');
       return;
     }
+
+    const fragment = document.createDocumentFragment();
+    Object.values(tasksList).forEach(task => {
+      const li = listItemTemplate(task);
+    });
+  }
+
+  function listItemTemplate({ _id, title, body } = {}) {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
+
+    const span = document.createElement('span');
+    span.textContent = title;
+    span.style.fontWeight = 'bold';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete task';
+    deleteBtn.classList.add('btn', 'danger', 'ml-auto', 'delete-btn');
+
+    const article = document.createElement('p');
+    article.textContent = body;
+    article.classList.add('mt-2', 'w-100');
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    li.appendChild(article);
+    console.log(li);
   }
 })(tasks);
